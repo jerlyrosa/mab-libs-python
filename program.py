96e29b8b0d90b03c52e5f6f184b312ad50bacc09
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-08-17 12:03:12 -0400

    first commit

---
+++ program.py
@@ -0,0 +1,9 @@
+adj = input("Adjetivo: ") # asombroso
+verbo1 = input("Verbo: ") # resolver
+verbo2 = input("Verbo: ") # programar
+sustantivo_plural = input("Sustantivo (plural): ") # metas, objetivos
+
+madlib = f"¡Programar es algo tan {adj}! Siempre me emociona puesto que me encanta {verbo1} problemas. \
+¡Si quieres aprende a {verbo2} con The Odin Proyect y alcanza tus {sustantivo_plural}!"
+
+print(madlib)
